f7c4d7a671e3fba0336c23af0b32e8e58810f3e7
Gabriel Antunes <127216398+antunes-pp@users.noreply.github.com>
2024-06-13 17:05:23 -0300

    init learning

---
+++ src/main/java/org/example/basic/Variable.java
@@ -0,0 +1,21 @@
+package org.example.basic;
+
+public class Variable {
+    public static void main(String[] args) {
+        int day = 13;
+        int month = 6;
+        int year = 2024;
+
+        System.out.println(day);
+        System.out.println(month);
+        System.out.println(year);
+
+        day = 22;
+        month = 12;
+        year = 1996;
+
+        System.out.println(day);
+        System.out.println(month);
+        System.out.println(year);
+    }
+}
