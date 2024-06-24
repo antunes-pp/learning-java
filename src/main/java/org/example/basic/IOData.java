ae12ccfa75f746a529b4148acea9748fbab74688
Gabriel Antunes <127216398+antunes-pp@users.noreply.github.com>
2024-06-24 14:51:55 -0300

    add another example

--- src/main/java/org/example/basic/IOData.java
+++ src/main/java/org/example/basic/IOData.java
@@ -3,18 +3,20 @@ package org.example.basic;
 import java.util.Scanner;
 
 public class IOData {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Insert a integer number:");
-        int userValue1 = scanner.nextInt();
+    System.out.println("Insert a integer number:");
+    int userValue1 = scanner.nextInt();
 
-        System.out.println("Insert another integer number:");
-        int userValue2 = scanner.nextInt();
+    System.out.println("Insert another integer number:");
+    int userValue2 = scanner.nextInt();
 
-        scanner.close();
+    System.out.println("----- Calculating -----");
 
-        System.out.printf("Total: %d\n", userValue1 + userValue2);
-        System.out.printf("Total in decimal: %f", (float) userValue1 / userValue2);
-    }
+    scanner.close();
+
+    System.out.printf("Total: %d\n", userValue1 + userValue2);
+    System.out.printf("Total in decimal: %f", (float) userValue1 / userValue2);
+  }
 }
