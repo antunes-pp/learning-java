0b45b81506a9cef2c31c0d7f12ab99ca83dd6abd
Gabriel Antunes <127216398+antunes-pp@users.noreply.github.com>
2024-06-24 09:49:48 -0300

    learn casting in java

--- src/main/java/org/example/basic/Casting.java
+++ src/main/java/org/example/basic/Casting.java
@@ -3,13 +3,20 @@ package org.example.basic;
 public class Casting {
     public static void main(String[] args) {
         // Implicit
-        short tinyValue = 1;
-        int space = tinyValue;
+        int space = (short) 1;
 
         // Narrowing
         long longValue = 232;
         space = (int) longValue;
 
         System.out.println(space);
+
+        // Division
+        int valOne = 9;
+        int valTwo = 2;
+
+        float result = (float) valOne / valTwo;
+
+        System.out.println(result);
     }
 }
