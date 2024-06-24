1e3d698e45838c2485b6fd0145092d00928ec044
Gabriel Antunes <127216398+antunes-pp@users.noreply.github.com>
2024-06-24 10:17:25 -0300

    feat: learn how to use i/o in java

---
+++ src/main/java/org/example/basic/IOData.java
@@ -0,0 +1,20 @@
+package org.example.basic;
+
+import java.util.Scanner;
+
+public class IOData {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insert a integer number:");
+        int userValue1 = scanner.nextInt();
+
+        System.out.println("Insert another integer number:");
+        int userValue2 = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.printf("Total: %d\n", userValue1 + userValue2);
+        System.out.printf("Total in decimal: %f", (float) userValue1 / userValue2);
+    }
+}
